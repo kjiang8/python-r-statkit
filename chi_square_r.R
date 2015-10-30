08813b8fb70c715a0fb80f0a127df92168d406ee
kjiang8 <kjiang@ctemc.org>
2015-10-30 09:00:56 -0400

    added column name, number in table

--- chi_square_r.R
+++ chi_square_r.R
@@ -1,40 +1,15 @@
-works <- function(){
-  M <- as.table(rbind(c(762, 327, 468), c(484, 239, 477)))
-  dimnames(M) <- list(gender = c("F", "M"),
-                      party = c("Democrat","Independent", "Republican"))
-  Xsq <- chisq.test(M)
-  print(Xsq)
-}
-
-
-readinteger <- function(n)
-{ 
-
-  if(!grepl("^[0-9]+$",n))
-  {
-    return("FALSE")
-  }
-  return(as.integer(n))
-}
-
-opencsv <- function()
+x2_gof <- function() #chi-square goodness of fit
 {
-  filename <- readline(prompt = "Enter .csv file name: ")
-  #global var
-  rawData <<- read.csv(file=paste0(filename,".csv"), head=TRUE, sep=",") #paste0 removes space
-  print(rawData)
-  rXsq <- chisq.test(rawData)
-  rXsq
-}
-
-x2_gof <- function()
-{
-
+  
   filename <- readline(prompt = "Enter .csv file name: ")
   
+  
   #reads titles
-  coltitles <<- read.csv(file=paste0(filename,".csv"), nrows=1, head=FALSE) #paste0 removes space
-  print(coltitles)
+  coltitles <- read.csv(file=paste0(filename,".csv"), nrows=1, head=FALSE)
+  newcol <- t(coltitles)
+  colnames(newcol) <- c('Col Names')
+  
+  print(newcol)
   
   f_obs <- readline(prompt = "Select observed frequency column by name or number: ")
   
@@ -44,12 +19,10 @@ x2_gof <- function()
   }
   
   f_exp <- readline(prompt = "Select expected frequency column by name or number. If evenly distributed, leave blank: ")
-  print(f_exp)
   
-  rawData <<- read.csv(file=paste0(filename,".csv"), head=TRUE, sep=",")
+  rawData <- read.csv(file=paste0(filename,".csv"), head=TRUE, sep=",")
   
   if (f_exp==""){
-  
     #evenly distributed data
     rXsq <- chisq.test(rawData[f_obs])
     print(rXsq)
@@ -68,19 +41,21 @@ x2_gof <- function()
     print(rXsq)
     
   }
-
+  
 }
 
-x2_ind <- function()
+x2_ind <- function() #chi-square test for independence
 {
   filename <- readline(prompt = "Enter .csv file name: ")
   
   #reads titles
-  coltitles <<- read.csv(file=paste0(filename,".csv"), nrows=1, head=FALSE) #paste0 removes space
-  print(coltitles)
+  coltitles <- read.csv(file=paste0(filename,".csv"), nrows=1, head=FALSE)
+  newcol <- t(coltitles)
+  colnames(newcol) <- c('Col Names')
+  
+  print(newcol)
   
   colns <- readline(prompt = "Select column(s) by name or number: ")
-  
   colns <- unlist(strsplit(colns,","))
   
   #test if letter or number
@@ -88,14 +63,13 @@ x2_ind <- function()
     colns <- as.integer(colns)
   }
   
-  rawData <<- read.csv(file=paste0(filename,".csv"), head=TRUE, sep=",")
+  rawData <- read.csv(file=paste0(filename,".csv"), head=TRUE, sep=",")
 
   #select = c(1:4)
   data <- subset(rawData, select=c(colns))
-
-  print(data)
   rXsq <- chisq.test(data)
   print(rXsq)
   
 }
+
 
